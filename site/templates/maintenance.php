789681ee28f02508373816165998d6934e8ed7e6
Loïc Horellou <hello@loichorellou.net>
2018-11-27 20:37:13 +0100

    update recherches + animation listener

--- site/templates/maintenance.php
+++ site/templates/maintenance.php
@@ -85,6 +85,30 @@
 
 	})
 
+	// var element = document.getElementById("watchme");
+	// element.addEventListener("animationstart", listener, false);
+	// element.addEventListener("animationend", listener, false);
+	// element.addEventListener("animationiteration", listener, false);
+
+	// element.className = "slidein";
+	// function listener(event) {
+	// 	var l = document.createElement("li");
+	// 	switch(event.type) {
+	// 		case "animationstart":
+	// 		l.innerHTML = "Début : durée écoulée : " + event.elapsedTime;
+	// 		break;
+	// 		case "animationend":
+	// 		l.innerHTML = "Fin : durée écoulée : " + event.elapsedTime;
+	// 		break;
+	// 		case "animationiteration":
+	// 		l.innerHTML = "Nouvelle boucle démarrée à : " + event.elapsedTime;
+	// 		break;
+	// 	}
+	// 	document.getElementById("output").appendChild(l);
+	// }
+
+
+
 </script>
 
 <!-- fin maintenance.php -->
